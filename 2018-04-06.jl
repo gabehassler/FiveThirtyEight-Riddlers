fa3e9773e8595fe7441f7163a69de79ffdb8d0f6
Gabe Hassler <gabeh12345@gmail.com>
2018-04-06 15:50:11 -0700

    Removed unnecessary variables/functions and included benchmarking

--- 2018-04-06.jl
+++ 2018-04-06.jl
@@ -1,4 +1,4 @@
-using JLD
+using JLD, BenchmarkTools
 
 function parse_games()
     cd("C:\\Users\\gabeh\\OneDrive\\Documents\\Programming\\Riddlers")
@@ -8,21 +8,17 @@ function parse_games()
     n = length(lines)
     dates = Array{Date}(n)
     teams = Array{String}(n, 2)
-    scores = Array{Int64}(n, 2)
     for i = 1:n
         line = lines[i]
         year = parse(line[1:4])
         month = parse(line[6:7])
         day = parse(line[9:10])
         team1 = strip(line[13:36])
-        score1 = parse(line[37:39])
         team2 = strip(line[42:65])
-        score2 = parse(line[66:68])
         dates[i] = Date(year, month, day)
         teams[i, 1:2] = [team1, team2]
-        scores[i, 1:2] = [score1, score2]
     end
-    save("ParsedNCAA.jld", "dates", dates, "teams", teams, "scores", scores)
+    save("ParsedNCAA.jld", "dates", dates, "teams", teams)
 end
 
 function get_teams_list(teams::Array{String, 2})
@@ -31,24 +27,26 @@ function get_teams_list(teams::Array{String, 2})
     return list
 end
 
-function find_champions(team::String, teams::Array{String, 2}, scores::Array{Int64, 2}, champions::Array{String, 1})
-    games_lost = findin(teams[:, 2], [team])
-    new_champs = teams[:, 1][games_lost]
-    actually_new = setdiff(new_champs, champions)
-    champions = vcat(champions, actually_new)
+function find_champions(team::String, teams::Array{String, 2}, champions::Array{String, 1})
+    games_lost = findin(teams[:, 2], [team]) #finds the indices for the games the team lost
+    new_champs = teams[:, 1][games_lost] #gets the team names of the winners of those games
+    actually_new = setdiff(new_champs, champions) #ensures that you haven't already accounted for any of the winners
+    champions = vcat(champions, actually_new) #merges the previous champions with the new ones
     for champ in actually_new
-        new_champs = find_champions(champ, teams, scores, champions)
+        new_champs = find_champions(champ, teams, champions) #recursively finds the teams that beach each new transitive champ
         champions = union(champions, new_champs)
     end
     return champions
 end
 
-
+println("Parsing")
 parse_games()
 d = load("ParsedNCAA.jld")
 date = d["dates"]
 teams = d["teams"]
-scores = d["scores"]
 teams_list = get_teams_list(teams)
 champions = ["Villanova"]
-champs = find_champions("Villanova", teams, scores, champions)
+champions = find_champions("Villanova", teams, champions)
+println("Number of Teams that are NOT Champions or Transitive Champions:")
+println(length(teams_list) - length(champions))
+display(@benchmark find_champions("Villanova", teams, champions))
